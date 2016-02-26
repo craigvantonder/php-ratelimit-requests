48c6bc31efde08732a70bc962dd9ff6ab3ebc88f
craigvantonder <craig@compulutions.co.za>
2016-02-26 04:14:51 +0200

    Updated test, readme

--- test.php
+++ test.php
@@ -1,17 +1,17 @@
 <?php
 # Include the class code
 require_once('ratelimitRequests.php');
-// Initialise / Construct the class
+# Initialise / Construct the class
 $rate = new ratelimitRequests ();
 # Evaluate with 3 requests every 10 seconds and if it exceeds then timeout for one minutes
-$ratelimit = $rate->evaluate(3, 10, 60);
-# The request passed enumeration
-if ($ratelimit) {
-  printf('This is a successful request!');
+$ratelimited = $rate->evaluate(3, 10, 60);
+# This attempted request is on a timeout
+if ($ratelimited) {
+  printf('This is an unsuccessful request, please try again in 1 minute!');
   exit;
 }
-// This attempted request is on a timeout
+# The request passed enumeration
 else {
-  printf('This is an unsuccessful request, please try again in 1 minute!');
+  printf('This is a successful request!');
   exit;
 }
